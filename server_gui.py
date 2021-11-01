c48f21d09234e73b296d801e77db73fb1da7e4cd
Lomakin Sergey <sergeylomakinals@mail.ru>
2021-11-01 15:41:13 +0500

    server_gui.py done

--- server_gui.py
+++ server_gui.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5.QtWidgets import QMainWindow, QDialog, QApplication, QAction, qApp, QLabel, QTableView, QPushButton, \
-    QMessageBox
+    QMessageBox, QLineEdit, QFileDialog
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
@@ -129,19 +129,78 @@ class ConfigWindow(QDialog):
 
     def initUI(self):
         # настройки окна
+        self.setFixedSize(365, 260)
+        self.setWindowTitle('Настройки сервера')
+
         # надпись о файле БД
+        self.db_path_label = QLabel('Путь до базы данных: ', self)
+        self.db_path_label.move(10, 10)
+        self.db_path_label.setFixedSize(240, 15)
+
         # строка с путём БД
+        self.db_path = QLineEdit(self)
+        self.db_path.setFixedSize(250, 20)
+        self.db_path.move(10, 30)
+        self.db_path.setReadOnly(True)
+
         # кнопка выбора пути
+        self.db_path_select = QPushButton('Обзор...', self)
+        self.db_path_select.move(275, 28)
+
         # функция обработчик открытия окна выбора папки
+        def open_file_dialog():
+            global dialog
+            dialog = QFileDialog(self)
+            path = dialog.getExistingDirectory()
+            path = path.replace('/', '\\')
+            self.db_path.insert(path)
+
+        self.db_path_select.clicked.connect(open_file_dialog)
+
         # метка с именем поля файла БД
+        self.db_file_label = QLabel('Имя файла базы данных: ', self)
+        self.db_file_label.move(10, 68)
+        self.db_file_label.setFixedSize(180, 15)
+
         # поле ввода имени файла
+        self.db_file = QLineEdit(self)
+        self.db_file.move(200, 66)
+        self.db_file.setFixedSize(150, 20)
+
         # метка с номером порта
-        # воле ввода порта
+        self.port_label = QLabel('Номер порта для соединений:', self)
+        self.port_label.move(10, 108)
+        self.port_label.setFixedSize(180, 15)
+
+        # поле ввода порта
+        self.port = QLineEdit(self)
+        self.port.move(200, 108)
+        self.port.setFixedSize(150, 20)
+
         # метка с адресом для соединения
+        self.ip_label = QLabel('С какого IP принимаем соединения:', self)
+        self.ip_label.move(10, 148)
+        self.ip_label.setFixedSize(180, 15)
+
         # метка с напоминанием о пустом поле
+        self.ip_label_note = QLabel(' оставьте это поле пустым, чтобы\n принимать соединения с любых адресов.', self)
+        self.ip_label_note.move(10, 168)
+        self.ip_label_note.setFixedSize(500, 30)
+
         # поле ввода ip
+        self.ip = QLineEdit(self)
+        self.ip.move(200, 148)
+        self.ip.setFixedSize(150, 20)
+
         # кнопка сохранения настроек
+        self.save_btn = QPushButton('Сохранить', self)
+        self.save_btn.move(190, 220)
+
         # кнопка закрытия окна
+        self.close_button = QPushButton('Закрыть', self)
+        self.close_button.move(275, 220)
+        self.close_button.clicked.connect(self.close)
+
         # показать окно
         self.show()
 
